dfd28cb43667b21a5fd2ec235058f6891ed8aef6
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-09-22 19:11:35 +0200

    Created structure for card game

--- CardGameUI/Program.cs
+++ CardGameUI/Program.cs
@@ -6,10 +6,66 @@ using System.Threading.Tasks;
 
 namespace CardGameUI
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
+
+            Console.ReadLine();
         }
+    }
+
+    public abstract class Deck
+    {
+        public void CreateDeck()
+        {
+        }
+
+        public virtual void ShuffleDeck()
+        {
+
+        }
+
+        public abstract List<PlayingCard> DealCard();
+
+        public virtual PlayingCard RequestCard()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    public class PlayingCard
+    {
+        public CardSuit Suit { get; set; }
+
+        public CardValue Value { get; set; }
+
+    }
+
+    public enum CardSuit
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
+    }
+
+    public enum CardValue
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+
     }
 }
